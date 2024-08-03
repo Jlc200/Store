1e3496312de927d2008e7e0265e46be93ce2d497
Jlc200 <109319510+Jlc200@users.noreply.github.com>
2024-08-03 01:28:45 -0500

    Add files via upload

---
+++ stock.js
@@ -0,0 +1,55 @@
+let stockProductos = [
+
+{
+   id: 3,
+   categoria:"lacteos",
+		 marca: "LECHE ENTERA GLORIA Lata 395g",
+		 imagen: "https://plazavea.vteximg.com.br/arquivos/ids/25967959-512-512/20345561.jpg",
+		 precio: "4.5",
+		 disponible: "DISPONIBLE: 20", 
+		 detalles: ["LECHE","RECONSTITUIDA ENTERA", "GLORIA 395g",]
+	},
+	{
+    id: 2,
+    categoria: "limpieza", 
+		 marca: "EXACTO GAS",
+		 imagen: "https://lh3.googleusercontent.com/p/AF1QipMNSm16OUE9yA6n0PrBnRpSK8iWHTFU8wF6YpfB=w768-h768-n-o-v1",
+		 precio: "",
+		 disponible: "AGOTADO", 
+		 detalles: ["GAS GLP","10 Kilos", "",]
+	},
+	{
+		 id: 1,
+		 categoria: "limpieza", 
+		 marca: "SOL GAS",
+		 imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVPvq0RQ0sDxYPi6Pedh6Vvk8loDnZjZAKwg&usqp=CAU",
+		 precio: "",
+		 disponible: "DISPONIBLE: 16", 
+		 detalles: ["GAS GLP","10 Kilos", "",]
+	},
+
+]
+
+let sliderProd = [
+   
+ {
+		 id: 1,
+		 marca: "REALIZAMOS RECARGAS VIRTUALES",
+		 imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwC_9gjqcyAazEuOkgqz31eb2LfebrVuYVkQ&usqp=CAU"
+	},
+ {
+		 id: 2,
+		 marca: "PAGO DE SERVICIOS",
+		 imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS099OCJxNCONxeV1J7EqGyXzY3Xh6vL2Fo7A&usqp=CAU"
+	},
+	{
+	id: 3,
+		 marca: "PUEDES REALIZAR TU PEDIDO Y PAGAR CON YAPE O PLIN",
+		 imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1VX3dPOAwf1Vld9UEIGD7dkqxzPRjQqfpJA&usqp=CAU"
+	},
+		{
+	id: 4,
+		 marca: "PAGAMOS TUS PRESTAMOS",
+		 imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpMofF3LkqjJnV-kzrq_7AMTsFUFMCPrnHDw&usqp=CAU"
+	},
+	]
